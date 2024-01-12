86defddf3b17ae47a6c4e05626b608bebff26ccc
Jagoda <109973414+toster3d@users.noreply.github.com>
2024-01-12 08:28:06 +0100

    Create Day_2.py

---
+++ Day_2.py
@@ -0,0 +1,8 @@
+# Create a program to calculate the area of a circle given its radius.
+
+import math
+
+def circle_area(r):
+    print(math.pi * r ** 2)
+
+circle_area(5)   # Output: 78.53981633974483
