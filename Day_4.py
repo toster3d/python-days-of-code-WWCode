f1d035999846cf88f63fd11e6699283b3d5632dc
Jagoda <109973414+toster3d@users.noreply.github.com>
2024-01-15 08:31:21 +0100

    Create Day_4.py

---
+++ Day_4.py
@@ -0,0 +1,9 @@
+# Write a program to find the maximum and minimum values in a list.
+
+
+def find_min_max(list_of_values):
+    print(f"The min value is: {min(list_of_values)}.\nThe max value is: {max(list_of_values)}.")
+
+
+find_min_max([1, 2, 3, 4, 5, 3, 9, 5, 0, 2])    #Output: The min value is: 0. The max value is: 9.
+
