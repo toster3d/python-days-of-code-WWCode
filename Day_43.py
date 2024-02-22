09b341ff277d907ef6a257715e1f12c5a3757ad7
Jagoda <109973414+toster3d@users.noreply.github.com>
2024-02-22 13:26:21 +0100

    Create Day_43.py

---
+++ Day_43.py
@@ -0,0 +1,7 @@
+# Write a program that removes all whitespaces from a given string.
+
+def remove_all_spaces(sentence):
+    return sentence.replace(' ', '')
+
+
+print(remove_all_spaces(" Python is the best programming language. "))
