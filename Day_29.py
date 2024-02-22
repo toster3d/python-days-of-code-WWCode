6514a7e31ca5bf77bda835a45ba90d06384a841f
Jagoda <109973414+toster3d@users.noreply.github.com>
2024-02-22 12:58:18 +0100

    Update Day_29.py

--- Day_29.py
+++ Day_29.py
@@ -3,7 +3,10 @@ import random
 
 
 def generate_random_number(start_range, stop_range):
-    print(random.randint(start_range, stop_range))
+    if start_range < stop_range:
+        print(random.randint(start_range, stop_range))
+    else:
+        print("The start number must be less than the stop number.")
 
 
 generate_random_number(5, 10)
