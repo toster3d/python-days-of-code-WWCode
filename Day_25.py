be9bd4a15853aa2ae43efc8c3c7070689fc36c03
Jagoda <109973414+toster3d@users.noreply.github.com>
2024-02-23 15:17:17 +0100

    Create Day_25.py

---
+++ Day_25.py
@@ -0,0 +1,10 @@
+# Create a program to concatenate two lists.
+
+def concatenated_lists(list_a, list_b):
+    final_list = list_a + list_b
+    return final_list
+
+
+A = [0, 1, 2, 3]
+B = [5, 6, 7]
+print(concatenated_lists(A, B))
